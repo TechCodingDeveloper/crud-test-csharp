f1e4d192cb33fb98a5d787ba5b7e3069d6110005
nozari <dev.nozari@gmail.com>
2023-01-30 11:13:46 +0330

    Create first Migration for Database and Create Test library
    - Create Tasebase class for implement structure test

---
+++ Mc2.CrudTest.AcceptanceTests/TastBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.AcceptanceTests
+{
+    public abstract class TestBase
+    {
+        //public Client ClientServices { get; set; }
+        public string BaseUrl { get; set; } = "http://localhost:5000/";
+        public TestBase()
+        {
+            var builder = Application.API.Program.CreateHostBuilder(Array.Empty<string>());
+            var host = builder.Build();
+            Task.Run(() => host.Run());
+
+            //ClientServices = new Client(BaseUrl, new HttpClient());
+        }
+    }
+}
---
+++ Mc2.CrudTest.Storage/Migrations/20230130074059_initialDatabase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mc2.CrudTest.Storage.Migrations
+{
+    public partial class initialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BankAccountNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModificationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Email",
+                table: "Customers",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_FirstName_LastName_DateOfBirth",
+                table: "Customers",
+                columns: new[] { "FirstName", "LastName", "DateOfBirth" },
+                unique: true,
+                filter: "[FirstName] IS NOT NULL AND [LastName] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
