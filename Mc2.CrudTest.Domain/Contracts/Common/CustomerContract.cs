350189ee449a00a83921e695b27715c33ae44490
nozari <dev.nozari@gmail.com>
2023-01-30 09:57:51 +0330

    Create library Domain
    - If you want to move data between models, you need to use this structure
    - Create CustomerContract transfer model data between CustomerEntity and Logic
    - Create  CustomerRequestContract transfer model data between CustomerContract and Service
    -Create  CustomerUpdateRequestContract transfer model data between  CustomerContract and Service

---
+++ Mc2.CrudTest.Domain/Contracts/Common/CustomerContract.cs
@@ -0,0 +1,49 @@
+﻿using Mc2.CrudTest.Storage.Database.Entities;
+using System;
+
+namespace Mc2.CrudTest.Domain.Contracts.Common
+{
+    public class CustomerContract
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccountNumber { get; set; }
+
+        public CustomerEntity Convert()
+        {
+            return new CustomerEntity()
+            {
+                Id = Id.HasValue ? Id.Value : default(long),
+                LastName = LastName,
+                FirstName = FirstName,
+                Email = Email,
+                BankAccountNumber = BankAccountNumber,
+                CreationDateTime = DateTime.Now,
+                DateOfBirth = DateOfBirth,
+                ModificationDateTime = DateTime.Now,
+                PhoneNumber = PhoneNumber,
+            };
+        }
+
+        public static CustomerContract ConvertContract(CustomerEntity customerEntity)
+        {
+            if (customerEntity is null)
+                return null;
+
+            return new CustomerContract()
+            {
+                Id = customerEntity.Id,
+                LastName = customerEntity.LastName,
+                FirstName = customerEntity.FirstName,
+                Email = customerEntity.Email,
+                BankAccountNumber = customerEntity.BankAccountNumber,
+                DateOfBirth = customerEntity.DateOfBirth,
+                PhoneNumber = customerEntity.PhoneNumber,
+            };
+        }
+    }
+}
---
+++ Mc2.CrudTest.Domain/Contracts/Requests/CustomerRequestContract.cs
@@ -0,0 +1,41 @@
+﻿using Mc2.CrudTest.Domain.Contracts.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mc2.CrudTest.Domain.Contracts.Requests
+{
+    public class CustomerRequestContract
+    {
+        [Required]
+        [MaxLength(450)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(450)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        [Phone]
+        [MaxLength(32)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string BankAccountNumber { get; set; }
+
+        public CustomerContract Convert()
+        {
+            return new CustomerContract()
+            {
+                BankAccountNumber = BankAccountNumber,
+                DateOfBirth = DateOfBirth,
+                Email = Email,
+                FirstName = FirstName,
+                LastName = LastName,
+                PhoneNumber = PhoneNumber,
+            };
+        }
+
+    }
+}
